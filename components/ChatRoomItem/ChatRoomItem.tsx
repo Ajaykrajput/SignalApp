6a48cadb6d0ea943c67fc5ec48121a2ea4707f3d
Ajay <ajayrajput7rcr@gmail.com>
2021-11-22 20:27:44 +0530

    ChatRoom Created

--- components/ChatRoomItem/ChatRoomItem.tsx
+++ components/ChatRoomItem/ChatRoomItem.tsx
@@ -1,21 +1,29 @@
 import React from "react";
-import { Text, Image, View, StyleSheet } from "react-native";
+import { Text, Image, View, Pressable } from "react-native";
+import { useNavigation } from "@react-navigation/core";
 import styles from "./styles";
 
 export default function ChatRoomItem({ chatRoom }) {
   // console.log(chatRoom);
   const user = chatRoom.users[1];
+  const navigation = useNavigation();
+  const onPress = () => {
+    console.warn("pressed on", user.name);
+    navigation.navigate("ChatRoom", { id: chatRoom.id });
+  };
   return (
-    <View style={styles.container}>
+    <Pressable onPress={onPress} style={styles.container}>
       <Image
         source={{
           uri: user.imageUri,
         }}
         style={styles.image}
       />
-      {chatRoom.newMessages && <View style={styles.badgeContainer}>
-        <Text style={styles.badgeText}>{chatRoom.newMessages}</Text>
-      </View> }
+      {chatRoom.newMessages && (
+        <View style={styles.badgeContainer}>
+          <Text style={styles.badgeText}>{chatRoom.newMessages}</Text>
+        </View>
+      )}
       <View style={styles.rightContainer}>
         <View style={styles.row}>
           <Text style={styles.name}>{user.name}</Text>
@@ -25,6 +33,6 @@ export default function ChatRoomItem({ chatRoom }) {
           {chatRoom.lastMessage.content}
         </Text>
       </View>
-    </View>
+    </Pressable>
   );
 }
