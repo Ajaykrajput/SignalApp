eebbcbd6eb21e0016b036fa9667c011ef978d58b
Ajay <ajayrajput7rcr@gmail.com>
2021-11-25 18:46:04 +0530

    new fetched chatrooms

--- components/ChatRoomItem/ChatRoomItem.tsx
+++ components/ChatRoomItem/ChatRoomItem.tsx
@@ -1,16 +1,36 @@
-import React from "react";
-import { Text, Image, View, Pressable } from "react-native";
+import React, { useState, useEffect } from "react";
+import { Text, Image, View, Pressable, ActivityIndicator } from "react-native";
 import { useNavigation } from "@react-navigation/core";
 import styles from "./styles";
+import { DataStore, Auth } from 'aws-amplify';
+import { ChatRoomUser, User } from "../../src/models";
 
 export default function ChatRoomItem({ chatRoom }) {
-  // console.log(chatRoom);
-  const user = chatRoom.users[1];
+  // const [users, setUsers] = useState<User[]>([]); // all users in this chatroom
+  const [user, setUser] = useState<User | null>(null); // the display user
+
   const navigation = useNavigation();
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const fetchedUsers = (await DataStore.query(ChatRoomUser))
+        .filter((chatRoomUser) => chatRoomUser.chatroom.id === chatRoom.id)
+        .map((chatRoomUser) => chatRoomUser.user);
+      // console.log(fetchedUsers);
+      // setUsers(fetchedUsers);
+      const authUser = await Auth.currentAuthenticatedUser();
+      setUser(fetchedUsers.find(user => user.id !== authUser.attributes.sub) || null);
+    };
+    fetchUsers();
+  }, []);
   const onPress = () => {
     // console.warn("pressed on", user.name);
     navigation.navigate("ChatRoom", { id: chatRoom.id });
   };
+
+  if (!user) {
+    return <ActivityIndicator />;
+  }
   return (
     <Pressable onPress={onPress} style={styles.container}>
       <Image
@@ -19,7 +39,7 @@ export default function ChatRoomItem({ chatRoom }) {
         }}
         style={styles.image}
       />
-      {chatRoom.newMessages && (
+      {!!chatRoom.newMessages && (
         <View style={styles.badgeContainer}>
           <Text style={styles.badgeText}>{chatRoom.newMessages}</Text>
         </View>
@@ -27,10 +47,10 @@ export default function ChatRoomItem({ chatRoom }) {
       <View style={styles.rightContainer}>
         <View style={styles.row}>
           <Text style={styles.name}>{user.name}</Text>
-          <Text style={styles.text}>{chatRoom.lastMessage.createdAt}</Text>
+          <Text style={styles.text}>{chatRoom.lastMessage?.createdAt}</Text>
         </View>
         <Text numberOfLines={1} ellipsizeMode="tail" style={styles.text}>
-          {chatRoom.lastMessage.content}
+          {chatRoom.lastMessage?.content}
         </Text>
       </View>
     </Pressable>
